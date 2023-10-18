97af702c5ece118fc33d67f8f536a41fa7085e63
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-10-18 21:04:26 +0530

    initial commit

---
+++ util/database.js
@@ -0,0 +1,5 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('expense-tracker', 'root', 'hulk@123', {dialect:'mysql', host:'localhost'});
+
+module.exports = sequelize;

b5fd4273bb03c70cb45fcacb5ad69bf0a36bb487
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-02 18:25:13 +0530

    adding user model

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const sequelize = require('../util/database');
+const Sequelize = require('sequelize');
+
+const Users = sequelize.define('users',{
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+    
+module.exports = Users;

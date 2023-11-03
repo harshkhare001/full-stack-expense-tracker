6d8bb177d42927ee21486c97d95d156b2eaa6dae
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-03 22:14:18 +0530

    Update expense.js

--- routes/expense.js
+++ routes/expense.js
@@ -3,14 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 const expressController = require('../controller/expense');
+const userAuthenticator = require('../middleware/auth');
 
 router.use(express.static("public"));
 
 router.get('/expense',expressController.getHomePage);
 
-router.get('/expenses', expressController.getExpenses);
+router.get('/expenses', userAuthenticator.authenticate, expressController.getExpenses);
 
-router.post('/addExpense', expressController.addExpense);
+router.post('/addExpense', userAuthenticator.authenticate, expressController.addExpense);
 
 router.get('/deleteExpense/:id',expressController.deleteExpense);
 

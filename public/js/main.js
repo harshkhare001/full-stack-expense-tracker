6d2cef90207f32d523de6a43ca536f3963e5228f
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-03 22:13:49 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,37 @@ var form = document.getElementById("form");
 
 var list = document.getElementById("list");
 
+form.addEventListener('submit', addItem);
+
+async function addItem(e){
+  e.preventDefault();
+  var name = document.getElementById('name').value;
+  var amount = document.getElementById('amount').value;
+  var category = document.getElementById('expense').value;
+
+  try
+  {
+    const token = localStorage.getItem("token");
+    let expense = {
+      name: name,
+      amount: amount,
+      expense: category
+    }
+    await axios.post("http://localhost:3000/addExpense",expense,{headers: {Authorization : token}});
+    window.location.reload();
+  }
+  catch(err)
+  {
+    console.log(err);
+  }
+
+}
+
 async function renderList() {
 
   try {
-    const expenses = await axios.get("http://localhost:3000/expenses");
+    const token = localStorage.getItem("token");
+    const expenses = await axios.get("http://localhost:3000/expenses",{ headers: { Authorization: token }});
     expenses.data.forEach((expense) => {
 
       var li = document.createElement("li");
@@ -14,10 +41,10 @@ async function renderList() {
 
       li.className = "items";
       deleteBtn.className = "delete btn btn-dark";
-
+      editBtn.className = "edit btn btn-info"
       editBtn.id = expense.id;
       deleteBtn.id = expense.id;
-      deleteBtn.appendChild(document.createTextNode("Delete"));
+      deleteBtn.appendChild(document.createTextNode("Delete Expense"));
       editBtn.appendChild(document.createTextNode("Edit"));
       let span1 = document.createElement("span");
       span1.textContent = `${expense.name}  `;
@@ -29,6 +56,7 @@ async function renderList() {
       li.appendChild(span2);
       li.appendChild(span3);
       li.appendChild(deleteBtn);
+      //li.appendChild(editBtn);
       list.appendChild(li);
     });
   } catch (e) {
@@ -39,15 +67,17 @@ window.addEventListener("DOMContentLoaded", () => {
   renderList();
 });
 
-async function del(e) {
+
+
+list.addEventListener("click", async function del(e){
+  e.preventDefault();
   if (e.target.classList.contains("delete")) {
     var li = e.target.parentElement;
     let id = e.target.id;
     await axios.get(`http://localhost:3000/deleteExpense/${id}`);
     list.removeChild(li);
   }
-}
-
-list.addEventListener("click", (e) => {
-  del(e);
+  else if(e.target.classList.contains("edit")){
+    
+  }
 });

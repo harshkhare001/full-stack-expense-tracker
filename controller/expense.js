aff1e5b0adfb3cc0915f3cbebfd5d36b7baaa4e8
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-03 22:13:06 +0530

    Update expense.js

--- controller/expense.js
+++ controller/expense.js
@@ -6,7 +6,8 @@ exports.getHomePage = (req,res,next)=>{
 }
 
 exports.getExpenses = (req,res,next)=>{
-    Expense.findAll().then((expenses)=>{
+    Expense.findAll({ where: { userId: req.user.id } })
+    .then((expenses)=>{
         res.json(expenses);
     })
     .catch((err)=>console.log((err)));
@@ -19,21 +20,26 @@ exports.addExpense = (req,res,next)=>{
     Expense.create({
         name:name,
         amount:amount,
-        expense:expense
-    }).then((result)=>{
+        expense:expense,
+        userId: req.user.id
+    })
+    .then((result)=>
+    {
         console.log('expense added');
-        res.redirect('/');
-    }).catch((err)=>console.log(err));
+        res.redirect('/expense');
+    })
+    .catch((err)=>console.log(err));
 }
 
 exports.deleteExpense = (req,res,next)=>{
     const Expenseid = req.params.id;
-    Expense.findByPk(Expenseid).then((expense)=>{
+    Expense.findByPk(Expenseid)
+    .then((expense)=>{
         return expense.destroy();
     })
     .then((result)=>{
         console.log('deleted');
-        res.redirect('/');
+        res.redirect('/expense');
     })
     .catch((err)=>console.log(err));
 }

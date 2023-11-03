391c0f5a0439fb39cd33b6256a97536a6c01e92d
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-03 16:05:23 +0530

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -13,7 +13,16 @@ async function login(e){
     };
     try{
         const res = await axios.post("http://localhost:3000/login", users);
-        console.log(res);
+        console.log(res.data.message);
+        if(res.data.message === 'Login Successful!')
+        {
+            alert(res.data.message);
+            window.location.href = "expense";
+        }
+        else if(res.data.message === 'Incorrect Password')
+        {
+            alert('Incorrect password')
+        }
     }
     catch(e){
         console.log(e);

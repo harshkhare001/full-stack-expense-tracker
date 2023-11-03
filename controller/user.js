db52de772dd4055f44cce0172ef44be569e5136e
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-03 13:16:41 +0530

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -5,6 +5,10 @@ exports.getSignUpPage = (req,res,next)=>{
     res.sendFile(path.join(__dirname, "../", "public", "views", "signup.html"))
 }
 
+exports.getLoginPage = (req,res,next)=>{
+    res.sendFile(path.join(__dirname, "../", "public", "views", "login.html"))
+}
+
 exports.addUser = (req,res,next)=>{
     console.log(req.body);
     const name = req.body.name;
@@ -18,7 +22,32 @@ exports.addUser = (req,res,next)=>{
     })
     .then((result)=>{
         console.log('User Added');
-        res.status(201).json(result);
+        res.status(200).json(result);
+    })
+    .catch((err)=>{
+        res.status(500).json(err);
+        console.log(err);
+    })
+}
+
+exports.login = (req,res,next)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    users.findOne({ where : { email : email}})
+    .then((user)=>{
+        if(user)
+        {
+            if(user.password === password){
+                res.status(200).json({ success: true, message: "Login Successful!" });
+            }
+            else {
+                res.status(401).json({success : false, message:"Incorrect Password"});
+            }
+        }
+        else{
+            res.status(404).json({success:false, message: "User Not Found"});
+        }
     })
     .catch((err)=>{
         console.log(err);

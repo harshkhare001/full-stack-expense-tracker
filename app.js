c41b24056a97245318c1562c27d8875f84ded6cc
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-04 14:49:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const User = require('./models/user');
 const Expense = require('./models/expense');
+const Order = require('./models/orders');
 
 const app = express();
 
@@ -13,11 +14,16 @@ app.use(bodyParser.json());
 
 const expenseRoute = require('./routes/expense');
 const userRoute = require('./routes/user');
+const purchaseRoute = require('./routes/purchase');
 
 app.use(expenseRoute);
 app.use(userRoute);
+app.use(purchaseRoute);
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
 
 sequelize.sync().then((result)=>app.listen(3000)).catch((err)=>console.log(err));

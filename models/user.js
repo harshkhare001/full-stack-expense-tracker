79fcac56dc17dd65fd5932c730c4faae694cff8c
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-05 14:48:11 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,22 +3,31 @@ const Sequelize = require('sequelize');
 
 const Users = sequelize.define('users',{
     id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+        type : Sequelize.INTEGER,
+        primaryKey : true,
+        allowNull :false,
+        autoIncrement : true,
     },
     name: {
-        type: Sequelize.STRING,
-        allowNull: false
+        type : Sequelize.STRING,
+        allowNull : false
     },
     email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : true
     },
     password: {
         type: Sequelize.STRING,
         allowNull: false
+    },
+    ispremiumuser:{
+        type : Sequelize.BOOLEAN,
+        defaultValue : false
+    },
+    totalExpense: {
+        type : Sequelize.INTEGER,
+        defaultValue : 0
     }
 });
     

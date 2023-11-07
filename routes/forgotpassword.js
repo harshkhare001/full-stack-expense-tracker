e65a299d6fc7fa43d2d7a4064c140aca11cc6b64
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-07 18:21:29 +0530

    Update forgotpassword.js

--- routes/forgotpassword.js
+++ routes/forgotpassword.js
@@ -8,6 +8,10 @@ router.use(express.static("public"));
 
 router.get('/forgotpassword',expressController.getHomePage);
 
-router.post('/password/forgotpassword', expressController.getEmail);
+router.post('/password/forgotpassword', expressController.sendEmail);
+
+router.get('/resetpassword/:uu_id', expressController.getResetPasswordPage);
+
+router.post('/password/resetpassword', expressController.changePassword);
 
 module.exports = router;

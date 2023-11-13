d5dc8b92efc888a5e2c1e96ab7107269c100d129
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-13 13:32:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 const sequelize = require('./util/database');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 
 const User = require('./models/user');
 const Expense = require('./models/expense');
 const Order = require('./models/orders');
+const ForgotPasswordRequest = require('./models/forgotpasswordrequest');
+const downloadedContent = require('./models/downloadedfiles');
 
 const app = express();
 
@@ -17,12 +25,19 @@ const userRoute = require('./routes/user');
 const purchaseRoute = require('./routes/purchase');
 const premiumRoute = require('./routes/premium');
 const forgotpasswordRoute = require('./routes/forgotpassword');
+const reportRoutes = require('./routes/report');
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags : 'a'})
 
 app.use(expenseRoute);
 app.use(userRoute);
 app.use(purchaseRoute);
 app.use(premiumRoute);
 app.use(forgotpasswordRoute);
+app.use(reportRoutes);
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', {stream : accessLogStream}));
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
@@ -30,4 +45,10 @@ Expense.belongsTo(User);
 User.hasMany(Order);
 Order.belongsTo(User);
 
-sequelize.sync().then((result)=>app.listen(3000)).catch((err)=>console.log(err));
+User.hasMany(ForgotPasswordRequest);
+ForgotPasswordRequest.belongsTo(User);
+
+User.hasMany(downloadedContent);
+downloadedContent.belongsTo(User);
+
+sequelize.sync().then((result)=>app.listen(3000)).catch((err)=>console.log(err));   

79d9bd38ff7573cef555590afd921c673172b645
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-16 20:04:22 +0530

    Update expense.js

--- controller/expense.js
+++ controller/expense.js
@@ -14,7 +14,6 @@ exports.getExpenses = async (req,res,next)=>
     {
         const page = +req.query.page || 1;
         const limit = +req.query.limit    || 3;
-        //console.log(page);
         const startIndex = (page-1)*limit;
         const totalExpenses = await Expense.count({ where: { userId: req.user.id }});
         const expenses = await Expense.findAll({ where: { userId: req.user.id }, offset : startIndex, limit : limit })

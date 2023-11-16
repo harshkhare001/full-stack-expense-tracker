43f7369e023e37a3b88722e57a5c740d7634311e
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-16 20:05:23 +0530

    Update premium.js

--- controller/premium.js
+++ controller/premium.js
@@ -1,12 +1,14 @@
 const User = require('../models/user');
 
-exports.showLead = (req, res, next)=>{
-
+exports.showLead = (req, res, next)=>
+{
     if(req.user.ispremiumuser)
     {
         User.findAll({attributes: ["name", "totalExpense"]})
-        .then((users)=>{
-            users.sort(function (a, b) {
+        .then((users)=>
+        {
+            users.sort(function (a, b) 
+            {
                 return b.totalExpense - a.totalExpense;
             });
             res.json(users);

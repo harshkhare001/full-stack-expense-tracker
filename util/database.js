6646f78cc8fe82d14ff6a2779bc8d1649ee8afe1
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-16 20:19:06 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('expense-tracker', 'root', 'hulk@123', {dialect:'mysql', host:'localhost'});
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {dialect:'mysql', host:process.env.DATABASE_HOST});
 
 module.exports = sequelize;

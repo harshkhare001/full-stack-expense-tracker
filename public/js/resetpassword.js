d7dcea6fd44ac4b526a1b4020b0822eaaea787e5
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-16 20:14:25 +0530

    Add files via upload

---
+++ public/js/resetpassword.js
@@ -0,0 +1,33 @@
+const form  = document.getElementById('resetpasswordform');
+
+form.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    console.log('working');
+    const password = document.getElementById('psw').value;
+    const cnfpassword = document.getElementById('cnf-psw').value;
+
+    if(password!== cnfpassword)
+    {
+        window.alert('Password did not Match');
+    }
+    else{
+        const uu_id = window.location.pathname.split('/').pop();
+        console.log(uu_id);
+        const data = {
+            uu_id,
+            password
+        };
+        try{
+            const res = await axios.post('http://localhost:3000/password/resetpassword',data);
+            console.log(res);
+            window.alert(res.data.message);
+        }
+        catch(err)
+        {
+            console.log(err);
+        }
+    }
+});
+
+
+

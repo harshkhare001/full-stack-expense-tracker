80f9fcef77d00386d3c98a671c2bfaf8a7821c49
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-11-28 16:55:38 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('expense-tracker', 'harsh', 'harsh123', {dialect:'mysql', host:'database-2.ceo8uaj0yirw.ap-south-1.rds.amazonaws.com'});
+const sequelize = new Sequelize('expense-tracker', 'harsh', 'harsh123', {dialect:'mysql', host:'database-1.ceo8uaj0yirw.ap-south-1.rds.amazonaws.com'});
 
 module.exports = sequelize;
